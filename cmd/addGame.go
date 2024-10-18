7f9bd2fe21443723cfd7468e25775cf54a295061
Taylor Petrillo <taypetrillo@gmail.com>
2024-10-18 20:30:35 +0100

    refactor: extract handler function

--- cmd/addGame.go
+++ cmd/addGame.go
@@ -11,18 +11,20 @@ var addGameCmd = &cobra.Command{
 	Short: "Add the outcome of a game to update rankings",
 	Long: `Add the outcome of a game in the following format:
 team1 team1_score team2 team2_score`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) == 0 {
-			return errors.New("Expected an argument but did not receive one")
-		}
+	RunE: addGameHandler,
+}
 
-		if len(args) != 4 {
-			return fmt.Errorf("Expected 4 arg, received %d args", len(args))
-		}
+func addGameHandler(cmd *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return errors.New("Expected an argument but did not receive one")
+	}
 
-		fmt.Println("TODO: Implement")
-		return nil
-	},
+	if len(args) != 4 {
+		return fmt.Errorf("Expected 4 arg, received %d args", len(args))
+	}
+
+	fmt.Println("TODO: Implement")
+	return nil
 }
 
 func init() {
